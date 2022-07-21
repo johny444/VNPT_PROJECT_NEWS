9d82f7d97f2fe0b00b2bd31423989e0f6788cbdd
Bùi Thành Lâm <btlam@vnptit3.vn>
2022-07-21 15:26:02 +0700

    xem danh sách chuyên đề

--- src/app/component/category/view/view.component.ts
+++ src/app/component/category/view/view.component.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
+  searchCategory = {
+    tenChuyenMuc: "",
+    maChuyenMuc: "",
+    tuNgayKhoiTao: "",
+    denNgayKhoiTao: ""
+  };
+
   categoryData!: any;
   searchCategoryData!: any;
-  constructor(private api:ApiService) { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.getCategoryDetail();
@@ -21,5 +28,75 @@ export class ViewComponent implements OnInit {
         this.categoryData = res;
         this.searchCategoryData = res;
       })
+  }
+
+  // searchh() {
+  //   this.searchCategoryData = this.categoryData.filter((post: any) => {
+  //     if (this.searchCategory.tenChuyenMuc === '' ||
+  //       post.tenChuyenMuc.toLowerCase().includes(this.searchCategory.tenChuyenMuc.toLowerCase())) {
+  //       return post;
+  //     }
+  //     return null;
+  //   })
+  // }
+  // searchh2() {
+  //   this.searchCategoryData = this.categoryData.filter((post: any) => {
+  //     let date1 = new Date(post.ngayKhoiTao);
+  //     let date2 = new Date(this.searchCategory.tuNgayKhoiTao);
+  //     if (!Number.isFinite(date2.getTime()) || date1.getTime() > date2.getTime()) {
+  //       return post;
+  //     }
+  //     return null;
+  //   })
+  // }
+  // searchh3() {
+  //   this.searchCategoryData = this.categoryData.filter((post: any) => {
+  //     let date1 = new Date(post.ngayKhoiTao);
+  //     let date3 = new Date(this.searchCategory.denNgayKhoiTao);
+  //     if (!Number.isFinite(date3.getTime()) || date1.getTime() < date3.getTime()) {
+  //       return post;
+  //     }
+  //     return null;
+  //   })
+  // }
+
+  searchh4() {
+    console.log(this.searchCategory.tuNgayKhoiTao)
+    let searchTen: any[] = [];
+    let searchTuNgay: any[] = [];
+    let searchDenNgay: any[] = [];
+
+    this.categoryData.forEach((post: any) => {
+      if (this.searchCategory.tenChuyenMuc === '' ||
+        post.tenChuyenMuc.toLowerCase().includes(this.searchCategory.tenChuyenMuc.toLowerCase())) {
+        searchTen.push(post);
+      }
+
+      let date1 = new Date(post.ngayKhoiTao);
+      let date2 = new Date(this.searchCategory.tuNgayKhoiTao);
+      if (!Number.isFinite(date2.getTime()) || date1.getTime() > date2.getTime()) {
+        searchTuNgay.push(post);
+      }
+
+      let date3 = new Date(this.searchCategory.denNgayKhoiTao);
+      if (!Number.isFinite(date3.getTime()) || date1.getTime() < date3.getTime()) {
+        searchDenNgay.push(post);
+      }
+    })
+    let arr = this.unique([...searchTen, ...searchTuNgay])
+    this.searchCategoryData = this.unique([...arr, ...searchDenNgay]);
+  }
+  unique(arr: any[]) {
+    var formArr = arr.sort((a: { id: number; }, b: { id: number; }) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
+    var idArr: any[] = [];
+    var resultArr = [];
+    for (let i = 1; i < formArr.length; i++) {
+      if (formArr[i].id == formArr[i - 1].id
+        && !idArr.includes(formArr[i].id)) {
+        idArr.push(formArr[i].id);
+        resultArr.push(formArr[i])
+      }
+    }
+    return resultArr
   }
 }
