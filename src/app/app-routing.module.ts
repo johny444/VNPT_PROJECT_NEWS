e5aee2c3969a3960613776761845abf30f35bc76
leduc24 <108445529+leduc24@users.noreply.github.com>
2022-07-22 11:12:57 +0700

    lại xóa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './component/category/add/add.component';
 import { ViewComponent } from './component/category/view/view.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'category', pathMatch:'full'},
   {path:'category', component: ViewComponent},
-  {path:'article', component: ViewComponent}
+  {path:'article', component: ViewComponent},
+  {path:'category/add', component: AddComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
