ffda886dcac6b053943337d5fc4696dfe5683dde
Bùi Thành Lâm <btlam@vnptit3.vn>
2022-07-26 10:15:09 +0700

    bài viết cho chuyên mục

--- src/app/component/user/posts-by-category/posts-by-category.component.ts
+++ src/app/component/user/posts-by-category/posts-by-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/share/api.service';
 
 @Component({
   selector: 'app-posts-by-category',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsByCategoryComponent implements OnInit {
 
-  constructor() { }
+  // dữ liệu toàn bộ các bài viết
+  postsData!: any;
+  // dữ liệu 5 bài mới nhất của tác giả
+  newPostsData!: any;
+  // 3 bài viết nhiều lượt xem
+  eyesPosts:any[]=[]
+  // 3 chuyên mục mới nhất
+  randomCategory!:any;
+  // tên chuyên mục
+  categoryName!: any;
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getPostsDetail();
+    this.getCategoryDetail();
+    this.api.postByCategory.subscribe((val:any) =>{
+      this.categoryName  = val;
+    })
   }
 
+  getPostsDetail() {
+    this.api.getPosts()
+      .subscribe(res => {
+        this.postsData = res;
+        this.newPostsData = this.postsData.filter((a:any)=>a.tenChuyenMuc == this.categoryName)
+        this.newPostsData = this.newPostsData.slice(-5);
+        this.eyesPosts = this.postsData.sort((a:any,b:any)=>a.luotXem-b.luotXem);
+        this.eyesPosts = this.eyesPosts.slice(-3);
+      })
+    
+  }
+  getCategoryDetail() {
+    this.api.getCategory()
+      .subscribe(res => {
+        this.randomCategory = res.map((x:any)=>x.tenChuyenMuc).slice(-3);
+      })
+  }
+
+  authorPost(nguoiDang:any){
+    this.api.authorPost.next(nguoiDang);
+  }
 }
