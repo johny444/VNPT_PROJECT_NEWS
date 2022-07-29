df28249d6be251dd15331d294b67e1cf66ba401e
Bùi Thành Lâm <btlam@vnptit3.vn>
2022-07-29 11:02:08 +0700

    khởi tạo thư mục article và category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './component/category/add/add.component';
 import { ViewComponent } from './component/category/view/view.component';
+import { AccountInformationComponent } from './component/signin/account-information/account-information.component';
 import { LoginComponent } from './component/signin/login/login.component';
 import { RegisterComponent } from './component/signin/register/register.component';
 import { ResetpasswordComponent } from './component/signin/resetpassword/resetpassword.component';
@@ -12,6 +13,7 @@ import { HomeComponent } from './component/user/home/home.component';
 import { PostsByCategoryComponent } from './component/user/posts-by-category/posts-by-category.component';
 
 const routes: Routes = [
+  // phần khách hàng
   {path:'', redirectTo:'home', pathMatch:'full'},
   // trang chủ
   {path:'home', component: HomeComponent},
@@ -23,12 +25,25 @@ const routes: Routes = [
   {path:'allpost', component: AllPostComponent},
   // bài viết theo chuyên mục
   {path:'postsbycategory', component: PostsByCategoryComponent},
+  // phần quản trị
   // trang danh sách chuyên mục
   {path:'category', component: ViewComponent},
-  // trang danh sách tin bài
-  {path:'article', component: ViewComponent},
   // trang thêm chuyên mục
   {path:'category/add', component: AddComponent},
+  // trang sửa chuyên mục
+  {path:'category/change', component: AddComponent},
+  // trang chi tiết chuyên mục
+  {path:'category/detail', component: AddComponent},
+  // trang danh sách tin bài
+  {path:'article', component: ViewComponent},
+  // trang thêm tin bài
+  {path:'article/add', component: AddComponent},
+  // trang sửa tin bài
+  {path:'article/change', component: AddComponent},
+  // trang chi tiết tin bài
+  {path:'article/detail', component: AddComponent},
+  
+  // phần tài khoản quản trị
   {
     path: 'register', component: RegisterComponent
   },
@@ -37,6 +52,9 @@ const routes: Routes = [
   },
   {
     path: 'reset', component: ResetpasswordComponent
+  },
+  {
+    path: 'accountinformation', component: AccountInformationComponent
   }
 ];
 @NgModule({
--- src/app/share/UserServices/user.service.ts
+++ src/app/share/UserServices/user.service.ts
@@ -9,49 +9,49 @@ import { User } from '../interface/user';
   providedIn: 'root'
 })
 export class UserService {
-  
-  public isLogin = new BehaviorSubject<boolean>(false);
-  // true là đã đăng nhập, false là chưa đăng nhập
   public loggedIn = new BehaviorSubject<boolean>(true);
   currentLoggedIn = this.loggedIn.asObservable()
   public loggedOut = new BehaviorSubject<boolean>(false);
   currentLoggedOut = this.loggedOut.asObservable()
   public dropdown = new BehaviorSubject<boolean>(false);
   currentdropdown = this.dropdown.asObservable()
-  _name= new BehaviorSubject<string>('');
-  current_name=this._name.asObservable();
-  
-  constructor(private http: HttpClient, private router: Router) { }
+  // newww
+  _accounter = new BehaviorSubject<User>({
+    id:0,
+    username:"",
+    email:"",
+    password:""
+});
+  current_accounter = this._accounter.asObservable();
 
-  getLogin(){
-    return this.http.get<any>("http://localhost:3003/Users")
-    .pipe(map((res:any)=>res))
-  }
+  constructor(private http: HttpClient, private router: Router) { }
 
   Register(Regis: any) {
     return this.http.post<any>("http://localhost:3003/Users", Regis)
   }
+  // newww
   Signin(USER: User) {
     this.http.get<any>("http://localhost:3003/Users").subscribe(res => {
       const user = res.find((a: any) => {
-        return a.username === USER.username && a.password === USER.password
+        return a.username === USER.username && a.password === USER.password;
       })
       if (user) {
         alert('Login Successful')
         this.loggedIn.next(false);
         this.loggedOut.next(true)
         this.dropdown.next(true)
-        this._name.next(USER.username)
+        this._accounter.next(user)
         // this.signInForm.reset()
         this.router.navigate(["article"])
-        console.log("router",this.router.navigate)
-
         return;
       } else {
         alert("user not found or Incorrect password")
       }
     })
-
   }
-
+  // newww
+  putUser(data: any, id: number) {
+    return this.http.put<any>("http://localhost:3003/Users/" + id, data)
+      .pipe(map((res: any) => res))
+  }
 }
